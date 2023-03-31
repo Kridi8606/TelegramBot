1ba1703cba234a10e03e2cf502a8debd7e1d1381
Kridi8606 <www.fedakridi.ru@gmail.com>
2023-03-31 14:37:17 +0300

    Удаление лишнего

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import telebot
-from config import BOT_TOKEN, GROUP_ID, ADMINS_ID
+from config import BOT_TOKEN
 from database.db import User_database, Hidden_continuation_database
 from datetime import datetime
-from tools.new_mailing_message_parser import new_mailing_message_parser
-from tools.creating_button import creating_button
 from pathlib import Path
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -23,46 +21,15 @@ def welcome_start(message):
         print(ex)
 
 
-# @bot.message_handler(commands=['new_mailing'])
-# def welcome_start(message):
-#     try:
-#         if message.from_user.id in ADMINS_ID:
-#             result = new_mailing_message_parser(message.text)
-#             if result['BUTTON_TEXT'] is not None:
-#                 callback_database.add_button(result['SUB_EVENT'], result['NO_SUB_EVENT'], result['COND'])
-#                 callback = callback_database.get_last_button()
-#                 btn = creating_button(result['BUTTON_TEXT'], callback[0])
-#                 # print(db.get_users())
-#                 for user_id in db.get_users():
-#                     try:
-#                         bot.send_message(user_id[0], result['POST_TEXT'], reply_markup=btn, parse_mode='Markdown')
-#                     except Exception as ex:
-#                         print('Ops... Mailing error')
-#                         print(ex)
-#             else:
-#                 # print(db.get_users())
-#                 for user_id in db.get_users():
-#                     try:
-#                         bot.send_message(user_id[0], result['POST_TEXT'], parse_mode='Markdown')
-#                     except Exception as ex:
-#                         print('Ops... Mailing error')
-#                         print(ex)
-#     except Exception as ex:
-#         print('Ops... Global Mailing error')
-#         print(ex)
-
-
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     try:
-        # print(call)
         if callback_database.check_button(int(call.data)):
             callback, sub_event, no_sub_event, condition = callback_database.get_button(int(call.data))
             flag = True
             if condition != 'None':
                 for i in condition.split(';'):
                     try:
-                        # print(bot.get_chat_member(i, call.from_user.id))
                         if bot.get_chat_member(i, call.from_user.id).status == 'left':
                             flag = False
                     except:
@@ -82,7 +49,6 @@ def invite(message):
     try:
         flag = False
         try:
-            # print(bot.get_chat_member(message.chat.id, message.from_user.id))
             if bot.get_chat_member(message.chat.id, message.from_user.id).status == 'left':
                 flag = True
         except:

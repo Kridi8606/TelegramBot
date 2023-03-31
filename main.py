2fb8cec92a57af1c334bb569d02c404619642b4b
philimonova <philimonovavika@gmail.com>
2023-03-31 20:18:44 +0300

    добавлена возможность создания кнопки с ссылкой для рассылки

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import sqlite3
 import time
+import validators
 
 import telebot
 from telebot import types
+from telebot.handler_backends import State, StatesGroup
+from telebot.storage import StateMemoryStorage
 from config import BOT_TOKEN, DATABASE_FILE
 from database.db import User_database
 
-bot = telebot.TeleBot(BOT_TOKEN)
+state_storage = StateMemoryStorage()
+bot = telebot.TeleBot(BOT_TOKEN, state_storage=state_storage)
 db = User_database(DATABASE_FILE)
+
+
+class MyStates(StatesGroup):
+    neutral = State()
+    mailing_text = State()
+    adding_button = State()
+    button_text = State()
+    button_link = State()
+    final_approve = State()
 
 
 # Тут работаем с командой start
@@ -32,47 +45,121 @@ def invite(message):
 @bot.message_handler(commands=['startmailing'])
 def start_mailing(message):
     # здесь (возможно) будет проверка, админ ли пользователь, написавший команду
-    text = bot.send_message(message.chat.id, 'Пришлите текст рассылки')
-    bot.register_next_step_handler(text, approve_mailing)
+    bot.send_message(message.chat.id, 'Пришлите текст рассылки')
+    bot.set_state(message.from_user.id, MyStates.mailing_text, message.chat.id)
 
 
-def approve_mailing(message):
-    mail_text = message.text
-    bot.send_message(message.chat.id, f'Текст вашей рассылки - {mail_text}')
-    buttons = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    btn1 = types.KeyboardButton('Подтвердить')
-    btn2 = types.KeyboardButton('Отмена')
-    buttons.add(btn1, btn2)
-    bot.send_message(message.chat.id, text='Разослать? (Это может занять некоторое время)', reply_markup=buttons)
-    bot.register_next_step_handler(message, sendall, mail_text)
+@bot.message_handler(state=MyStates.mailing_text)
+def approve(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['mailing_text'] = message.text
+    bot.send_message(message.chat.id, f'Текст вашей рассылки-{message.text}')
+    btns = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Да')
+    btn2 = types.KeyboardButton('Нет')
+    btn3 = types.KeyboardButton('Отмена')
+    btns.add(btn1, btn2, btn3)
+    bot.send_message(message.chat.id, 'Добавить кнопку с ссылкой?', reply_markup=btns)
+    bot.set_state(message.from_user.id, MyStates.adding_button, message.chat.id)
 
 
-def sendall(message, mail_text):
+@bot.message_handler(state=MyStates.adding_button)
+def answer(message):
     removing_buttons = types.ReplyKeyboardRemove()
-    if message.text == 'Подтвердить':
-        try:
-            list_of_users = db.get_all_users()
-            for user in list_of_users:
-                bot.send_message(user[0], mail_text)
-                # у телеграмма вроде есть ограничение 30 сообщ/сек, так что на всякий случай добавила задержку
-                time.sleep(0.1)
-            bot.send_message(message.chat.id, text='Успех!', reply_markup=removing_buttons)
-        except sqlite3.Error:
-                print('Упс! Проблемы с соединением с базой данных')
-                bot.send_message(message.chat.id, 'Упс! Проблемы с соединением с базой данных', reply_markup=removing_buttons)
-        except Exception:
-            print('Кажется, что-то пошло не так...')
-            bot.send_message(message.chat.id, 'Кажется, что-то пошло не так...', reply_markup=removing_buttons)
+    if message.text == 'Да':
+        bot.send_message(message.chat.id, 'Отлично, теперь пришлите текст для кнопки', reply_markup=removing_buttons)
+        bot.set_state(message.from_user.id, MyStates.button_text, message.chat.id)
+    elif message.text == 'Нет':
+        bot.send_message(message.chat.id, 'Отлично, переходим к финальной проверке', reply_markup=removing_buttons)
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            data['button_text'] = ''
+            data['button_link'] = ''
+            final_approve(data, message.chat.id)
+        bot.set_state(message.from_user.id, MyStates.final_approve, message.chat.id)
     elif message.text == 'Отмена':
-        bot.send_message(message.chat.id, text='Вы отменили рассылку', reply_markup=removing_buttons)
+        bot.send_message(message.chat.id, 'Вы отменили рассылку', reply_markup=removing_buttons)
+        bot.delete_state(message.from_user.id, message.chat.id)
     else:
-        bot.send_message(message.chat.id, 'Ну вот, теперь начинайте заново', reply_markup=removing_buttons)
+        bot.send_message(message.chat.id, 'Пожалуйста, воспользуйтесь кнопками')
+
+
+@bot.message_handler(state=MyStates.button_text)
+def add_button_text(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['button_text'] = message.text
+    bot.send_message(message.chat.id, 'Теперь пришлите ссылку для кнопки')
+    bot.set_state(message.from_user.id, MyStates.button_link, message.chat.id)
+
+
+@bot.message_handler(state=MyStates.button_link)
+def add_button_link(message):
+    if not validators.url(message.text):
+        bot.send_message(message.chat.id, 'Это не ссылка')
+    else:
+        bot.set_state(message.from_user.id, MyStates.final_approve, message.chat.id)
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            data['button_link'] = message.text
+        final_approve(data, message.chat.id, button=True)
+
+
+def final_approve(data, chat_id, button=False):
+    buttons = None
+    if button:
+        buttons = types.InlineKeyboardMarkup()
+        b = types.InlineKeyboardButton(text=data['button_text'], url=data['button_link'])
+        buttons.add(b)
+    bot.send_message(chat_id, 'Вот как выглядит ваша рассылка:')
+    bot.send_message(chat_id, text=data['mailing_text'], reply_markup=buttons)
+    final_confirm_buttons = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    confirm_btn = types.KeyboardButton('Да')
+    cancel_btn = types.KeyboardButton('Отмена')
+    final_confirm_buttons.add(confirm_btn, cancel_btn)
+    bot.send_message(chat_id, 'Начать рассылку? (Это может занять некоторое время)', reply_markup=final_confirm_buttons)
+
+
+@bot.message_handler(state=MyStates.final_approve)
+def answer(message):
+    removing_btns = types.ReplyKeyboardRemove()
+    if message.text == 'Да':
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            mail_text = data['mailing_text']
+            btn_text = data['button_text']
+            btn_link = data['button_link']
+            sendall(message, mail_text, btn_text, btn_link)
+    elif message.text == 'Отмена':
+        bot.send_message(message.chat.id, 'Вы отменили рассылку', reply_markup=removing_btns)
+        bot.delete_state(message.from_user.id, message.chat.id)
+    else:
+        bot.send_message(message.chat.id, 'Пожалуйста, воспользуйтесь кнопками')
+
+
+def sendall(message, mail_text, btn_text, btn_link):
+    removing_buttons = types.ReplyKeyboardRemove()
+    mail_btns = None
+    if btn_text and btn_link:
+        mail_btns = types.InlineKeyboardMarkup(row_width=1)
+        mail_btn = types.InlineKeyboardButton(text=btn_text, url=btn_link)
+        mail_btns.add(mail_btn)
+    try:
+        list_of_users = db.get_all_users()
+        for user in list_of_users:
+            bot.send_message(user[0], text=mail_text, reply_markup=mail_btns)
+            # у телеграмма вроде есть ограничение 30 сообщ/сек, так что на всякий случай добавила задержку
+            time.sleep(0.1)
+        bot.send_message(message.chat.id, text='Успех!', reply_markup=removing_buttons)
+        bot.set_state(message.from_user.id, MyStates.neutral, message.chat.id)
+    except sqlite3.Error:
+        bot.send_message(message.chat.id, 'Упс! Проблемы с соединением с базой данных',
+                         reply_markup=removing_buttons)
+    except Exception:
+        bot.send_message(message.chat.id, 'Кажется, что-то пошло не так...', reply_markup=removing_buttons)
 
 
 # @bot.message.hamdler()
 # def leave(message):
 #     print(message)
 
+bot.add_custom_filter(telebot.custom_filters.StateFilter(bot))
 
 if __name__ == "__main__":
     print("Бот запущен")
